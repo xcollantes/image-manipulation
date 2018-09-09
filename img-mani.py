11c3b13bd78aff44d2f8d62285eba95de5a77ace
xcollantes <xcollantes@users.noreply.github.com>
2018-09-09 00:58:44 -0700

    Run test
    Test produced desired image.

--- img-mani.py
+++ img-mani.py
@@ -44,12 +44,18 @@ def flip(img):
 	
 
 def greenScreen(bg, gs):
+	# This operation takes a while, let's keep track with a timer 
+	
+	# Use Tableau to find green threshold value to be used below 
+	p = 'green_values.csv'
+	file = open(p, 'w+')
+	file.write("Red, Green, Blue\n")
+	
 	# Establish counter for green screen image 
 	WIDTH = gs.size[0] - 1
 	HEIGHT = gs.size[1] - 1
 	pos = (0, 0)
 	try:
-		
 		# Stop when smallest dimension is reached on green screen image 
 		while pos[0] <= WIDTH:
 		
@@ -64,13 +70,21 @@ def greenScreen(bg, gs):
 				else:
 					pass
 				
+				try:
+					o = str(gs.getpixel(pos)[0]) + ", " + str(gs.getpixel(pos)[1]) + ", " + str(gs.getpixel(pos)[2]) + "\n"
+					file.write(o)  # Output RGB values for analysis 
+				except IOError as ioe:
+					print("Error with file output %s" %ioe)
+					
 				pos = (pos[0], pos[1] + 1)  # Increment height by 1
 
 			pos = (pos[0], 0)
 			pos = (pos[0] + 1, pos[1])  # Increment width by 1
-	except Exception:
+	except Exception as e:
+		print("Iteration issue %s" %e)
 		pass
-		
+	
+	file.close()
 	gs.save(arrow.now('US/Pacific').format('DD-MMM-YY_HH-mm-SS') + '.png')
 	
 if __name__=='__main__':
