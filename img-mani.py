dc8f09e29ab4cc540d8f8a3a63c2330879df3f74
xcollantes <xcollantes@users.noreply.github.com>
2018-09-08 20:03:13 -0700

    Add photo

--- img-mani.py
+++ img-mani.py
@@ -2,4 +2,16 @@
 # @date: 09/8/2018
 # @content: Ch. 17 from Al Sweigart's "How to Automate the Boring Stuff with Python"
 
-import pillow
+from PIL import Image
+import os, sys
+
+def pillow():
+	img = Image.open('girls.jpg')
+	
+
+
+
+
+if __name__=='__main__':
+	os.chdir('lib/')
+	pillow()
